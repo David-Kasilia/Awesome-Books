662b21445a2628bf01750b95534d42a597a6a224
David Kasilia <83514256+David-Kasilia@users.noreply.github.com>
2022-08-02 23:43:14 +0300

    Complete project

--- script/script.js
+++ script/script.js
@@ -1,59 +1,97 @@
-const bookList = document.getElementById('bookList');
-const title = document.getElementById('title');
-const author = document.getElementById('author');
-const form = document.querySelector('form');
-
-const books = [];
-
-function storeBooks() {
-  localStorage.setItem('books', JSON.stringify(books));
-}
-
-function createBook(title, author) {
-  const newBook = {
-    title,
-    author,
-  };
-  books.push(newBook);
-
-  const singleBook = document.createElement('div');
-  books.forEach((book, index) => {
-    singleBook.innerHTML = `
-            <p>${book.title}</p>
-            <p>${book.author}</p>
-            <button id="remove">Remove</button>
-            <hr>
-        `;
-
-    const removeBtn = singleBook.querySelector('#remove');
-    removeBtn.addEventListener('click', () => {
-      books.splice(index, 1);
-      singleBook.remove();
-      storeBooks();
-    });
-  });
-
-  storeBooks();
-
-  bookList.appendChild(singleBook);
-}
-
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  if (title.value !== '' && author.value !== '') {
-    createBook(title.value, author.value);
-
-    title.value = '';
-    author.value = '';
+/* eslint-disable consistent-return */
+/* eslint max-classes-per-file: ["error", 5] */
+class BookList {
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
   }
+}
+
+class StoreBooks {
+  static getBooks() {
+    let books;
+
+    if (localStorage.getItem('books') === null) {
+      books = [];
+    } else {
+      books = JSON.parse(localStorage.getItem('books'));
+    }
+
+    return books;
+  }
+
+  static addBook(book) {
+    const books = StoreBooks.getBooks();
+    books.push(book);
+    localStorage.setItem('books', JSON.stringify(books));
+  }
+
+  static removeBook(title) {
+    const books = StoreBooks.getBooks();
+
+    books.forEach((book, index) => {
+      if (book.title === title) {
+        books.splice(index, 1);
+      }
+    });
+
+    localStorage.setItem('books', JSON.stringify(books));
+  }
+}
+
+class UI {
+  static displayBooks() {
+    const books = StoreBooks.getBooks();
+
+    books.forEach((book) => UI.addBookToList(book));
+  }
+
+  static addBookToList(book) {
+    const bookList = document.querySelector('#bookList');
+
+    const listContainer = document.createElement('div');
+
+    listContainer.innerHTML = `
+    <p class="bookDetails">"${book.title}" by ${book.author}  <button class="remove">Remove</button></p>
+    `;
+
+    bookList.appendChild(listContainer);
+  }
+
+  static deleteBook(el) {
+    if (el.classList.contains('remove')) {
+      el.parentElement.remove();
+    }
+  }
+
+  static clearFields() {
+    document.getElementById('title').value = '';
+    document.getElementById('author').value = '';
+  }
+}
+
+document.addEventListener('DOMContentLoaded', UI.displayBooks);
+
+document.querySelector('#bookForm').addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const title = document.getElementById('title').value;
+  const author = document.getElementById('author').value;
+
+  if (title === '' || author === '') {
+    return false;
+  }
+  const newBook = new BookList(title, author);
+
+  UI.addBookToList(newBook);
+
+  StoreBooks.addBook(newBook);
+
+  UI.clearFields();
 });
 
-function retrieveBooks() {
-  const books = JSON.parse(localStorage.getItem('books'));
-  if (books) {
-    books.forEach((book) => {
-      createBook(book.title, book.author);
-    });
-  }
-}
-retrieveBooks();
+document.querySelector('#bookList').addEventListener('click', (e) => {
+  UI.deleteBook(e.target);
+
+  StoreBooks.removeBook(e.target.parentElement.previousElementSibling.textContent);
+});
